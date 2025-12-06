c7cc8857ce1c53f56c105b7a6e8336702569042e
deepaksp24 <96986851+deepaksp24@users.noreply.github.com>
2025-12-06 20:26:45 +0530

    added log for better debugging

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import eventlet
+
+eventlet.monkey_patch()
 from flask import Flask, request
-from flask_socketio import SocketIO, join_room, emit
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app,async_mode='eventlet', cors_allowed_origins="*",logger=True, engineio_logger=True)
 
 # in-memory doc storage (simple)
 documents = {}
+
+@app.route("/", methods=["GET"])
+def create():
+    return {"status": True, "message": "sever on"}
 
 @app.route("/create", methods=["POST"])
 def create_doc():
@@ -24,7 +31,7 @@ def handle_edit(data):
     doc_id = data["doc_id"]
     content = data["content"]
     documents[doc_id] = content
-    emit("update", {"content": content}, room=doc_id, include_self=False)
+    emit("update", {"content": content}, room=doc_id, include_self=True)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000,debug=True)
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-RUN THIS IN PROJECT DIRECTORY 
+RUN THIS IN PROJECT DIRECTORY
 
 python -m venv venv
 venv/scripts/activate
 python app.py
+flask run --debug --host <ur ip>
